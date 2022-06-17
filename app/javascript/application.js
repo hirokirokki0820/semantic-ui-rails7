9f3f12fc8299c57014174bd95bcb2e11fbedb064
hirokirokki0820 <hirokirokki0820@gmail.com>
2022-06-17 16:04:27 +0900

    Add how to set up Semantic-UI for Rails7

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,25 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+// Loads all Semantic javascripts
+//= require semantic-ui
+
 import "@hotwired/turbo-rails"
 import "controllers"
+import "jquery"
+
+/* jQuery for Semantic UI  */
+$(function(){
+
+  /* ドロップダウン */
+  $('.ui.dropdown').dropdown();
+
+  /* アコーディオン */
+  $('.ui.accordion').accordion();
+
+  /* ポップアップ */
+  $('.activating.element').popup();
+  $('.button').popup({
+    inline: true
+  });
+
+});
